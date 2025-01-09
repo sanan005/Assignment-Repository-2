b4ac38aca1a41d618a51c757b1dc1bd9bab3c07f
sanan005 <sananmarakkar19@gmail.com>
2025-01-09 20:19:49 +0400

    vendingmachine
    This is for the second assignment based on a model for a  vending machine .
    
    The conditions and preferences are mentioned before hand and we are supposed to make it based on those instructions.
    
    additional information is also given complimentary options to be added in the code.

---
+++ vendingmachine.py
@@ -0,0 +1,9 @@
+ #first we make a dictionary of items in the vending machine
+items = {
+    'A': {'name': 'Coke', 'category': 'Drink', 'price': 2.50, 'stock': 10},
+    'B': {'name': 'Pepsi', 'category': 'Drink', 'price': 2.50, 'stock': 5},
+    'C': {'name': 'Chips', 'category': 'Snack', 'price': 4.00, 'stock': 8},
+    'D': {'name': 'Chocolate', 'category': 'Snack', 'price': 6.25, 'stock': 12},
+    'E': {'name': 'Coffee', 'category': 'Hot Drink', 'price': 5.00, 'stock': 5},
+    'F': {'name': 'esspreso', 'category': 'Hot Drink', 'price': 4.50, 'stock': 5},
+}
